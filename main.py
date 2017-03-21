74b7bb60cf560dd9ec68304d039e11e670f79d49
Jozo <jozo.kovac@gmail.com>
2017-03-21 13:55:45 +0100

    typo

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from generate import Generator
 generator = Generator(50000)
 generator.generate()
 #generator.print()
-generator.save(absolute_timestamp=True)
+generator.save(absolute_timestamps=True)
 generator.track('9e898732-a289-11e6-bc55-14187733e19e', absolute_timestamps=True)
