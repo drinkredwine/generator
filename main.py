729725cc1b7baa30cfbfe3f7b873c0901146dcdc
Jozo <jozo.kovac@gmail.com>
2017-03-22 20:00:00 +0100

    version with local serialisation

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from generate import Generator
+import pickle
+from time import time
 
 # 5k is probably a limit of customer API
-generator = Generator(1000)
-generator.generate()
-#generator.print()
-# generator.save(absolute_timestamps=True)
+
+#
+# start = time()
+# generator = Generator(50000)
+# print("generated in {}s".format(time()-start))
+# generator.generate()
+# #generator.print()
+#
+# start = time()
+# with open("generator.bin", "wb") as f:
+#     pickle.dump(generator, f)
+# print("saved in {}s".format(time()-start))
+
+start = time()
+with open("generator.bin", "rb") as f:
+    generator = pickle.load(f)
+print("loaded in {}s".format(time() - start))
+
+# print(generator.users[0])
+# print(generator.users[0])
+
 generator.track('9e898732-a289-11e6-bc55-14187733e19e', absolute_timestamps=True)
+# generator.save(absolute_timestamps=True)
